a1f549ce19aac280491bc5f0bcc6ecc7ffb587fb
pi22-a <pi22a555@naver.com>
2022-10-17 10:03:34 +0900

    피라미드 별찍기

---
+++ src/Like_Lion/Day1017/algorithm/Pyramid.java
@@ -0,0 +1,29 @@
+package Like_Lion.Day1017.algorithm;
+
+public class Pyramid {
+    public void printstar(int n, String a) {
+
+        for(int i = 0; i<n; i++){
+            for (int j = 1; j< n-i; j++) {
+                System.out.printf(" ");
+            }
+            for (int j = 0; j<(2*i)+1; j++){
+                System.out.printf(a);
+            }
+
+            System.out.printf("\n");
+        }
+    }
+
+    public static void main(String[] args) {
+        Pyramid pr = new Pyramid();
+        pr.printstar(5,"*");
+    }
+}
+
+/*
+   *
+  * *
+ * * *
+* * * *
+ */
